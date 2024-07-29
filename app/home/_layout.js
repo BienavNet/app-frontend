05f90ead4136b8ed352e2640a47e7650a47b4356
Carlos Sanchez Carrascal <carlosstqm2225@gmail.com>
2024-07-29 18:03:57 -0500

    more changes

--- app/home/_layout.js
+++ app/home/_layout.js
@@ -1,40 +1,29 @@
 import { Stack } from "expo-router/stack";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { ToastProvider } from "react-native-toast-notifications";
 import { AuthProvider, AuthContext } from "../../src/context/JWTAuthContext";
 import Loading from "../../components/share/loading";
 import { useAuth } from "../../src/hooks/useAuth";
 
 //home
 export default LayoutHome = () => {
-  const { nombre } = useAuth().user
-  console.log("nombre", nombre);
-  const namewelcome = `Bienveido ${nombre}`
+  const { nombre } = useAuth().user;
+  const namewelcome = `Bienveido ${nombre}`;
   return (
     <AuthProvider>
       <AuthContext.Consumer>
-        {(auth) => (!auth.isInitialized ? <Loading /> :
-       <>
-        <Stack screenOptions={
-          {
-            title:namewelcome,
-          }
+        {(auth) =>
+          !auth.isInitialized ? (
+            <Loading />
+          ) : (
+            <>
+              <Stack
+                screenOptions={{
+                  title: namewelcome,
+                }}
+              />
+            </>
+          )
         }
-        />
-        </>
-      )}
       </AuthContext.Consumer>
     </AuthProvider>
-    // <Authenticated>
-    //   <Slot/>
-    // </Authenticated>
-    // <ToastProvider>
-    // <AuthProvider>
-    //   <Stack />
-    //   {/* <AuthContext.Consumer>
-    //     {(auth) => (!auth.isInitialized ? <Loading /> : <Stack />)}
-    //   </AuthContext.Consumer> */}
-    // </AuthProvider>
-    // </ToastProvider>
   );
 };
